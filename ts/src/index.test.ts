ef25ee08da6d650db07a55d01df58ea5bfcf3fb1
Dario Pranjic <derioreal63@gmail.com>
2026-03-27 20:20:37 +0100

    Fix test tooling

---
+++ ts/src/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("index", () => {
+    // mock test
+    it("should pass", () => {
+        // compare true to true
+        expect(true).toBe(true);
+    });
+}); 
