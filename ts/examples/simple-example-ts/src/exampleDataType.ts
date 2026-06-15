b109ebbdec1d77458bddf53d64fa77f69c045a5f
nicosammito <nsammito@code0.tech>
2026-06-16 00:46:45 +0200

    feat: remove old files

--- ts/examples/simple-example-ts/src/exampleDataType.ts
+++
@@ -1,6 +0,0 @@
-import {HerculesDataType} from "@code0-tech/hercules";
-
-export const someDataType: HerculesDataType = {
-    identifier: "SOME_DATATYPE",
-    type: "any",
-}
--- ts/examples/simple-example-ts/src/exampleEventType.ts
+++
@@ -1,7 +0,0 @@
-import {HerculesEventType} from "@code0-tech/hercules"
-
-export const someEventType: HerculesEventType = {
-    signature: "(): string",
-    editable: false,
-    identifier: "test_flow",
-}
