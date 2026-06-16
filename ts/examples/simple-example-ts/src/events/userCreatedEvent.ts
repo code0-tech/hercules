8629544e2c5e8692803b790b340aefb35936b7c7
nicosammito <nsammito@code0.tech>
2026-06-16 13:05:42 +0200

    feat: updating example by removing unnecessary setting override

--- ts/examples/simple-example-ts/src/events/userCreatedEvent.ts
+++ ts/examples/simple-example-ts/src/events/userCreatedEvent.ts
@@ -4,10 +4,4 @@ import {UserCreatedRuntimeEvent} from "./userCreatedRuntimeEvent.ts";
 @Identifier("user_created_event")
 @Name({code: "en-US", content: "On User Created"})
 @Editable(false)
-@EventSetting({
-    identifier: "FILTER_ROLE",
-    name: [{code: "en-US", content: "Role Filter"}],
-    description: [{code: "en-US", content: "Only trigger for users with this role"}],
-    optional: true,
-})
 export class UserCreatedEvent extends UserCreatedRuntimeEvent {}
