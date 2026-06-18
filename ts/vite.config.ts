8e75129ffbca39f0542dca59511d93572d1a7a68
nicosammito <nsammito@code0.tech>
2026-06-18 15:18:36 +0200

    feat: adjusting build process and adding definitions building to build

--- ts/vite.config.ts
+++ ts/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import { builtinModules } from 'module';
 
 export default defineConfig({
     build: {
+        target: 'node18',
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'triangulum',
@@ -12,10 +14,11 @@ export default defineConfig({
         },
         rollupOptions: {
             external: [
+                ...builtinModules,
+                ...builtinModules.map(m => `node:${m}`),
                 'typescript',
                 '@code0-tech/tucana',
-                'path',
-                'fs'
+                '@grpc/grpc-js',
             ],
             output: {
                 globals: {
